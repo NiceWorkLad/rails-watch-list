f73ba1d0e181276e0fc92007dafe9a32f8a7d1aa
louis whittleadams <louiswhittle7d1@gmail.com>
2024-08-23 10:31:36 +0100

    nested bookmark

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  resources :lists
-  resources :bookmarks
+  resources :lists do
+    resources :bookmarks
+  end
 end
